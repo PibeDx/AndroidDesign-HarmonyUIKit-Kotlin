faee1c838e514d036153fe8b785279ede9ea45a0
PibeDx <jcuentast@gmail.com>
2017-07-10 02:58:31 -0500

    feat(design): row discover: - add design row discover - add resourcer for row discover

---
+++ app/src/main/java/com/josecuentas/androiddesign_harmony_ui_kit_kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.josecuentas.androiddesign_harmony_ui_kit_kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
