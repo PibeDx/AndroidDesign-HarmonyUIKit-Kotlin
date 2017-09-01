bf4a5fb97d749b5250ce72c977f7563954f21e5b
José Cuentas Turpo <jcuentast@gmail.com>
2017-09-01 18:11:32 -0500

    feat(widget): custom seekbar:  - create CustomSeekBar  - add thumb for seekbar  - pending onDraw

--- app/src/main/java/com/josecuentas/androiddesign_harmony_ui_kit_kotlin/MainActivity.kt
+++ app/src/main/java/com/josecuentas/androiddesign_harmony_ui_kit_kotlin/MainActivity.kt
@@ -1,27 +1,62 @@
 package com.josecuentas.androiddesign_harmony_ui_kit_kotlin
 
+import android.content.res.Resources
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.AppCompatImageButton
+import android.util.Log
 import android.widget.RatingBar
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.view_filter.*
+
+
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var ratingBar : RatingBar
     lateinit var constraintLayout : ConstraintLayout
+    lateinit var seekBar : SeekBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         injectView()
+        events()
         ui()
+    }
+
+    private fun events() {
+        seekBar.thumbOffset = dpToPx(9)
+        seekBar.setPadding(0,0,0,0)
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Log.d("TAG", seekBar!!.thumbOffset.toString())
+                val number = (progress * (seekBar!!.width - 2 * seekBar!!.thumbOffset)) /
+                        seekBar.max
+                textView.text = number.toString()
+                textView.text = "${progress} %"
+                val margin = dpToPx(20) * 2
+                textView.x = seekBar.x + number +  seekBar.thumbOffset / 2
+                //textView.translationX =
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
     }
 
     private fun injectView() {
         ratingBar = findViewById(R.id.rbaPoint)
         constraintLayout = findViewById(R.id.constraintLayout)
+        seekBar = findViewById(R.id.seekBar)
     }
 
     private fun ui() {
@@ -44,5 +79,9 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+    }
+
+    protected fun dpToPx(dp: Int): Int {
+        return (dp.toFloat() * Resources.getSystem().getDisplayMetrics().density).toInt()
     }
 }
---
+++ app/src/main/java/com/josecuentas/androiddesign_harmony_ui_kit_kotlin/widget/CustomSeekbar.kt
@@ -0,0 +1,43 @@
+package com.josecuentas.androiddesign_harmony_ui_kit_kotlin.widget
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
+import android.support.v7.widget.AppCompatSeekBar
+import android.util.AttributeSet
+import android.util.Log
+import com.josecuentas.androiddesign_harmony_ui_kit_kotlin.R
+
+/**
+ * Created by jcuentas on 1/09/17.
+ */
+class CustomSeekbar : AppCompatSeekBar {
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        val thumb_x = (this.progress.toDouble() / this.max * this.width.toDouble()).toFloat()
+        val middle = this.height.toFloat() / 2
+
+        val paint = Paint()
+        val boundsThumb = this.thumb.bounds as Rect
+
+        Log.d("boundsThumb.toString()", boundsThumb.toString())
+        Log.d("boundsThumb.centerX()", boundsThumb.centerX().toString())
+        val thumbX = boundsThumb.centerX().toFloat()
+        val thumbY = boundsThumb.centerY().toFloat()
+
+        paint.setColor(Color.BLACK)
+        paint.setTextSize(resources.getDimension(R.dimen.seekbar_thumb_text_size))
+        //paint.textAlign = Paint.Align.CENTER
+        canvas!!.drawText("0",
+                thumbX/* + paint.measureText(this.progress.toString())/2*/,
+                thumbY,
+                paint)
+    }
+}
