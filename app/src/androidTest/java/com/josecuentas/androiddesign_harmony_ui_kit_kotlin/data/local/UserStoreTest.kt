3fc44bf39bc71f7d8ac73d481b6ef3a215976d72
PibeDx <jcuentast@gmail.com>
2017-09-10 12:30:53 -0500

    feat: login test:  - add test UserStoreTest

--- app/src/androidTest/java/com/josecuentas/androiddesign_harmony_ui_kit_kotlin/data/local/UserStoreTest.kt
+++ app/src/androidTest/java/com/josecuentas/androiddesign_harmony_ui_kit_kotlin/data/local/UserStoreTest.kt
@@ -1,6 +1,7 @@
 package com.josecuentas.androiddesign_harmony_ui_kit_kotlin.data.local
 
 import android.support.test.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 
@@ -15,5 +16,25 @@ class UserStoreTest {
         val userStore = UserStore(context, null)
         assertNotNull(userStore)
         assertNotNull(userStore.users)
+        assertEquals(0, userStore.users.size)
+    }
+
+    @Test
+    fun count() {
+        val context = InstrumentationRegistry.getTargetContext()
+        val userStore = UserStore(context, "login")
+        assertNotNull(userStore)
+        assertNotNull(userStore.users)
+        assertEquals(1, userStore.users.size)
+    }
+
+    @Test
+    fun getUserPibeDx() {
+        val context = InstrumentationRegistry.getTargetContext()
+        val userStore = UserStore(context, "login")
+        val user = userStore.get("pibedx")
+        assertNotNull(user)
+        assertEquals("pibedx", user!!.username)
+        assertEquals("José Norberto", user!!.name)
     }
 }
